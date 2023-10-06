7a7f6f3dc2812ae0592f6a867e147fb862f5a76b
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-10-07 02:06:24 +0530

    fix: update api endpoints

--- components/Form.tsx
+++ components/Form.tsx
@@ -25,7 +25,7 @@ export default function Test() {
     setErrorMessage("");
 
     try {
-      const res = await fetch("http://localhost:3000/api/users", {
+      const res = await fetch("/api/users", {
         method: "POST",
         headers: {
           "Content-type": "application/json",
