95d82724c5bc9efa562c0fbc4b7073fa6a9cc6a0
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-10-07 17:29:39 +0530

    style: change fonts

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           Sikkim Flood Relief Connect
         </h1>
         <p className="mb-6 text-md">
-          Connecting familes during Sikkim floods—facilitating connections,
+          Connecting familes during Sikkim floods—facilitating connections, <br />
           rescue efforts, and support for affected families.
         </p>
         <a href="#find-people" className="bg-green-400 px-8 py-2 rounded mr-4">
@@ -20,12 +20,12 @@ export default function Home() {
 
       <div className="max-w-6xl mx-auto px-4 py-8 sm:px-6 lg:px-8 min-h-[60vh] grid grid-cols-6 gap-4">
         <div className="col-span-6 md:col-span-2 p-2">
-          <h3 className="text-2xl font-semibold mb-4">Submit Information</h3>
+          <h3 className="text-2xl mb-4">Submit Information</h3>
           <Form />
         </div>
 
         <div id="find-people" className="col-span-6 md:col-span-4 p-2">
-          <h3 className="text-2xl font-semibold mb-4">Trapped Peoples</h3>
+          <h3 className="text-2xl mb-4">Trapped Peoples</h3>
           <UsersContainer />
         </div>
       </div>
