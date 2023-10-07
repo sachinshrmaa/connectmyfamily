d085de2d134e3891c9e18e1e8b7c4e453fec2e6b
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-10-07 23:47:47 +0530

    fix: update credentials in env file

--- utils/firebase.js
+++ utils/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWR9EJkS8eoWtoG_NjaCSlqk-drvOmhMo",
-  authDomain: "connectmyfamilysikkim.firebaseapp.com",
-  projectId: "connectmyfamilysikkim",
-  storageBucket: "connectmyfamilysikkim.appspot.com",
-  messagingSenderId: "963462490987",
-  appId: "1:963462490987:web:6b96405147597cbcd4fa2e",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
