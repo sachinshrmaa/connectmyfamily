262e96b3b65305a359b8063aac6941601a76e0ae
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-10-07 23:56:37 +0530

    fix: add default user image

--- models/User.ts
+++ models/User.ts
@@ -15,6 +15,8 @@ const userSchema = new mongoose.Schema({
   image: {
     type: String,
     required: true,
+    default:
+      "https://firebasestorage.googleapis.com/v0/b/connectmyfamilysikkim.appspot.com/o/images%2Fdefualt_user_image.jpg?alt=media&token=83f0a374-e844-4a93-b75f-7ce37642715a&_gl=1*mos2zz*_ga*Njc1MTQxNDUuMTY5NjY5Nzg5NA..*_ga_CW55HF8NVT*MTY5NjY5NzkxNS4xLjEuMTY5NjcwMzA2MS4yNi4wLjA.",
   },
 });
 
