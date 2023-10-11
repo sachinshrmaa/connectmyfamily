f7c7ebd366bdec168e9976211b2d6cd17b78ae5d
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-10-12 00:53:07 +0530

    feat: update the user schema

--- models/User.ts
+++ models/User.ts
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema({
     default:
       "https://firebasestorage.googleapis.com/v0/b/sikkimconnectmyfamily.appspot.com/o/images%2Fdefault_img.png?alt=media&token=cc6ce384-3bb0-4a1e-81ca-c83219feee1f&_gl=1*992oe1*_ga*Njc1MTQxNDUuMTY5NjY5Nzg5NA..*_ga_CW55HF8NVT*MTY5NzA0Nzg5OC41LjEuMTY5NzA1MDg2NS40MC4wLjA.",
   },
+  createdAt: {
+    type: Date,
+    default: Date.now, // Set the default value to the current date and time
+  },
 });
 
 const User = mongoose.models.UserDB || mongoose.model("UserDB", userSchema);
